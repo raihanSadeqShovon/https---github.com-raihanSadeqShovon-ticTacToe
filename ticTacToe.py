3dc9c0bff6db31262c6ad99691f5719c68913fd2
raihan-sadeq <raihanshovon@yahoo.com>
2022-10-11 00:31:20 +0600

    Complete game

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,66 +1,96 @@
 import itertools
-game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
 
-#horizontally winner
-for row in game:
-    #print(row)
-    if row.count(row[0]) == len(row) and row[0] != 0:
-        print(f"Player {row[0]} is horizontally winner")
+def win(current_game):
 
-#vertically winner
-for col in range(len(game[0])):
-    check = []
+    def all_same(p):
+        if p.count(p[0]) == len(p) and p[0] != 0:
+            return True
+        else:
+            return False
+
+    #horizontally winner
     for row in game:
-        check.append(row[col])
-    if check.count(check[0]) == len(check) and check[0] != 0:
-        print(f"Player {check[0]} is vertically winner")
+        #print(row)
+        if all_same(row):
+            print(f"Player {row[0]} is horizontally winner")
+            return True
 
-# \ diagonally winner
-diag = []
-for cross in range(len(game)):
-    diag.append(game[cross][cross])
-if diag.count(diag[0]) == len(diag) and diag[0] != 0:
-    print(f"Player {diag[0]} is diagonally winner \\")
+    #vertically winner
+    for col in range(len(game[0])):
+        check = []
+        for row in game:
+            check.append(row[col])
+        if check.count(check[0]) == len(check) and check[0] != 0:
+            print(f"Player {check[0]} is vertically winner")
+            return True
 
-# / diagonnaly winner
-diag = []
-for idx, rev_idx in enumerate(reversed(range(len(game)))):
-    diag.append(game[idx][rev_idx]) 
+    # \ diagonally winner
+    diag = []
+    for cross in range(len(game)):
+        diag.append(game[cross][cross])
+    if diag.count(diag[0]) == len(diag) and diag[0] != 0:
+        print(f"Player {diag[0]} is diagonally winner \\")
+        return True
 
-if diag.count(diag[0]) == len(diag) and diag[0] != 0:
-    print(f"Player {diag[0]} is diagonally winner /")
+    # / diagonnaly winner
+    diag = []
+    for idx, rev_idx in enumerate(reversed(range(len(game)))):
+        diag.append(game[idx][rev_idx]) 
+
+    if diag.count(diag[0]) == len(diag) and diag[0] != 0:
+        print(f"Player {diag[0]} is diagonally winner /")
+        return True
+    return False
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
     try:
-        print("   0  1  2")
+        if game_map[row][column] != 0:
+            print("This position is occupied! Choose another!")
+            return game_map, False
+        print("   "+"  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][column] = player
+
         for count, row in enumerate(game_map):
             print(count, row)
-        return game_map
+
+        return game_map, True
+
     except IndexError:
         print("Did you attempt to play a row or column outside the range of 0,1 or 2? (IndexError)")
-        return False
+        return game_map, False
+
     except Exception as e:
         print(str(e))
-        return False
+        return game_map, False
 
 play = True
 players = [1, 2]
 while play:
-    game = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
 
+    game_size = int(input("What is the size of your game? "))
+    game = [[0 for i in range(game_size)] for i in range(game_size)]
     game_won = False
+    game, _ = game_board(game,just_display = True) 
     player_cycle = itertools.cycle(players)
-    game_board(game,just_display = True)
+    
     while not game_won:
         current_player = next(player_cycle)
         print(f"Player: {current_player}")
-        column_choice = int(input("Which column? "))
-        row_choice = int(input("Which row? "))
-
-        game_board(game, player = current_player, row=row_choice, column=column_choice)
+        played = False
+        while not played:
+            column_choice = int(input("Which column do you want to play? (0, 1, 2): "))
+            row_choice = int(input("Which row do you want to play? (0, 1, 2): "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
+        
+        if win(game):
+            game_won = True
+            again = input("The game is over, would you like to play again? (y/n) ")
+            if again.lower() == "y":
+                print("Restarting")
+            elif again.lower() == "n":
+                print("Byeeee")
+                play = False
+            else:
+                print("Not a valid answer, see you later alegator")
+                play = False
